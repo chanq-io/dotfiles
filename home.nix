5a6e9baac3bf7c71232b1bda0d001e43fdd600ed
chanq-io <pierre@chanq.io>
2025-08-13 18:24:32 +0100

    try use nixgl

--- home.nix
+++ home.nix
@@ -1,11 +1,23 @@
-{ pkgs, inputs, system, ...}: {
+{ pkgs, inputs, system, lib, ...}: 
+let 
+  nixGL = import (pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "nixGL";
+    rev = "a8e1ce7d49a149ed70df676785b07f63288f53c5";
+    sha256 = "010n26nhha1l64m7m08rzrfhyixfr8japn3vzbn80dj092wwggrr";
+  }) { inherit pkgs; };
+  kitty-wrapped = pkgs.writeShellScriptBin "kitty" ''
+    exec ${nixGL.auto.nixGLDefault}/bin/nixGL ${pkgs.kitty}/bin/kitty "$@"
+  '';
+in
+{
   home.username = "cardamom";
   home.homeDirectory = "/home/cardamom";
   home.stateVersion = "25.11";
   home.packages = with pkgs; [
     eza
     inputs.zen-browser.packages."${system}".default
-    kitty
+    kitty-wrapped
     neofetch
   ];
 
@@ -14,4 +26,7 @@
   ];
 
   programs.home-manager.enable = true;
+  programs.kitty = {
+    enable = true;
+  };
 }
