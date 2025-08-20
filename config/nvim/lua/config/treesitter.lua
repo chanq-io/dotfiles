e4819a82d6f20e7b0028195c41af5c699321d1d3
chanq-io <pierre@chanq.io>
2025-08-20 12:43:21 +0100

    try change order

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
+    auto_install = false,
     ensure_installed = { "rust" }, 
     highlight = { enable = true },
     indent = { enable = true },
-    auto_install = false,
 })
