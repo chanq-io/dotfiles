dd90a7aa7780e0622c2ef95997271cdadf43b86a
chanq-io <pierre@chanq.io>
2025-08-20 16:24:40 +0100

    more treesitter

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup({
   auto_install = false,
+  parser_install_dir = vim.fn.stdpath("data") .. "/parsers",
   ensure_installed = { "rust" },
   highlight = { enable = true },
   indent = { enable = true },
