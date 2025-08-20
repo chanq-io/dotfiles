75fd2f291103470cc52bd5cc5d6d80670c4e73ea
chanq-io <pierre@chanq.io>
2025-08-20 12:42:29 +0100

    try a different field

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -2,5 +2,5 @@ require('nvim-treesitter.configs').setup({
     ensure_installed = { "rust" }, 
     highlight = { enable = true },
     indent = { enable = true },
-    parser_install_dir = "/dev/null",
+    auto_install = false,
 })
