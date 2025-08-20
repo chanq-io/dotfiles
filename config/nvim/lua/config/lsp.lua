2bf2c743f3385cc7015e87cd7af28b5babe2e719
chanq-io <pierre@chanq.io>
2025-08-20 16:21:06 +0100

    more treesitter nonsense

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -31,29 +31,20 @@ local opts = {
 }
 
 vim.g.rustaceanvim = {
-  -- Plugin configuration
-  tools = {
-  },
-  -- LSP configuration
+  tools = { },
   server = {
     on_attach = function(client, bufnr)
       -- you can also put keymaps in here
     end,
     default_settings = {
-      -- rust-analyzer language server configuration
       ['rust-analyzer'] = { },
     },
   },
-  -- DAP configuration
-  dap = {
-  },
-
+  dap = { },
   treesitter = {
     enable = true,
     install = false,
-    parser_path = function()
-      return vim.fn.stdpath("data") .. "/parsers/rust.so"
-    end
+    -- Remove the parser_path function - it's unnecessary
   },
 }
 
