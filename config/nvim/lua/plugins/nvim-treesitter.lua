af516f1cd19f7771720d8d29de534127375d419c
chanq-io <pierre@chanq.io>
2025-08-21 12:19:42 +0100

    more treesitter

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,28 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  -- event = { 'BufReadPost', 'BufNewFile' }, 
+  lazy = false,
   opts = function()
-    local root = vim.fn.stdpath('data') .. '/parsers'   
-    local so_dir = root .. '/parser'                    
+    local root = vim.fn.stdpath('data') .. '/parsers'
     return {
       auto_install = false,
-      ensure_installed = { 'rust' }, 
+      ensure_installed = { 'rust' },
       parser_install_dir = root,
       highlight = { enable = true },
       indent = { enable = true },
     }
   end,
   config = function(_, opts)
-    require('nvim-treesitter.install').compilers = { 'clang' }
+    require('nvim-treesitter.install').compilers = { 'clang', 'gcc', 'clang++', 'g++' }
+    require('nvim-treesitter.install').prefer_git = true
+
+    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+    if parser_config.rust and parser_config.rust.install_info then
+      parser_config.rust.install_info.files = { 'src/parser.c', 'src/scanner.cc', 'src/scanner.c' }
+    end
+
     require('nvim-treesitter.configs').setup(opts)
-    -- Make Neovim see the compiled parsers
+
     vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/parsers/parser')
   end,
 }
-
