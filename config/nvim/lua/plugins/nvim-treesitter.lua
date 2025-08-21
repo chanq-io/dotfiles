97ca24f3bf197b230a763289ce6b864b402114f4
chanq-io <pierre@chanq.io>
2025-08-21 11:42:59 +0100

    treesitter

---
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = { 'BufReadPost', 'BufNewFile' }, 
+  opts = function()
+    local root = vim.fn.stdpath('data') .. '/parsers'   
+    local so_dir = root .. '/parser'                    
+    return {
+      auto_install = false,
+      ensure_installed = { 'rust' }, 
+      parser_install_dir = root,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+  end,
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+    -- Make Neovim see the compiled parsers
+    vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/parsers/parser')
+  end,
+}
+
