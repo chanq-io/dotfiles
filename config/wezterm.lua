02f1c53cebbd0b5137abc00b1ae4134cf53799a5
chanq-io <pierre@chanq.io>
2025-10-10 22:12:13 +0100

    update fonts

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- or, changing the font size and color scheme.
+config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font_size = 10
 config.color_scheme = 'AdventureTime'
 
