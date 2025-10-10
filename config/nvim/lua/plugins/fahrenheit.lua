8960c49dfc0f86ba1e0a38ef5f2665f258fbfaf1
chanq-io <pierre@chanq.io>
2025-10-10 22:32:57 +0100

    update fahrenheit config

--- config/nvim/lua/plugins/fahrenheit.lua
+++ config/nvim/lua/plugins/fahrenheit.lua
@@ -1,8 +1,10 @@
 return {
-  "benjamin-kruger/fahrenheit-nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("fahrenheit")
-  end,
+    url = "https://github.com/benjamin-kruger/fahrenheit-nvim.git",
+    name = "fahrenheit-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("fahrenheit")
+    end,
 }
+
