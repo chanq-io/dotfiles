7d95189c971b1af4dacddd6961aa32b4974b3618
chanq-io <pierre@chanq.io>
2025-10-10 22:42:58 +0100

    update wezterm conf

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,15 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- or, changing the font size and color scheme.
 config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font_size = 10
 config.color_scheme = 'darkmatrix'
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false 
+config.hide_tab_bar_if_only_one_tab = true 
 
--- Finally, return the configuration to wezterm:
 return config
