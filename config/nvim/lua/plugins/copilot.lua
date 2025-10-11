427979cf3ba0b62fb3fd96ee3184b1f12ae8c2ea
chanq-io <pierre@chanq.io>
2025-10-11 21:32:16 +0100

    add copilot again

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+            suggestion = {
+            enabled = true,
+            auto_trigger = true,  
+            debounce = 75,
+            keymap = {
+              accept = "<C-CR>",
+              accept_word = false,
+              accept_line = false,
+              next = "<C-]>",
+              prev = "<C-[>",
+              dismiss = "<C-BS>",
+            },
+          },
+    })
+  end,
+}
