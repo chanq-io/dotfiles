487cbf89dcd7e5c40befec23e28621800a4f062e
chanq-io <pierre@chanq.io>
2025-10-11 23:58:22 +0100

    markdown toggle

--- config/nvim/lua/config/editing.lua
+++ config/nvim/lua/config/editing.lua
@@ -37,6 +37,8 @@ vim.g.rustfmt_fail_silently = false
 
 vim.opt.fillchars = { eob = ' ' }
 
+require("markdown-toggle").setup()
+
 vim.cmd [[
     let &t_SI = "\<Esc>]50;CursorShape=1\x7"
     let &t_EI = "\<Esc>]50;CursorShape=0\x7"
