11145e8c5b9dabb2bd4bafc4157e6892da5d5968
Pierre Chanquion <pierre@chanq.io>
2026-04-20 18:52:57 +0100

    feat: set zsh as cardamom's login shell

--- nixos/modules/nixos/users.nix
+++ nixos/modules/nixos/users.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
+
   users.users.cardamom = {
     isNormalUser = true;
     description = "Pierre Chanquion";
     extraGroups = [ "networkmanager" "wheel" "video" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [ ];
   };
 }
