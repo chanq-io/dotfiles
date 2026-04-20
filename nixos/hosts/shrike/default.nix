75ceaa0bc71a7cee2ef0671dd693d198adf54819
Pierre Chanquion <pierre@chanq.io>
2026-04-20 17:33:27 +0100

    refactor: extract locale settings into module

--- nixos/hosts/shrike/default.nix
+++ nixos/hosts/shrike/default.nix
@@ -4,30 +4,12 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/nixos/boot.nix
+    ../../modules/nixos/locale.nix
     ../../modules/nixos/networking.nix
     ../../modules/nixos/nix.nix
   ];
 
   networking.hostName = "shrike";
-
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   users.users.cardamom = {
     isNormalUser = true;
