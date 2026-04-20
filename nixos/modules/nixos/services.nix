9f9faf3b950a25e23824aa1b64fa23bee93e9059
Pierre Chanquion <pierre@chanq.io>
2026-04-20 23:20:18 +0100

    feat(nixos): add docker daemon and docker/audio groups

--- nixos/modules/nixos/services.nix
+++ nixos/modules/nixos/services.nix
@@ -7,4 +7,8 @@
   # (ghostty, wezterm, kitty, etc.). Without this, remote sessions fail
   # with "can't find terminal definition" when TERM isn't xterm-256color.
   environment.enableAllTerminfo = true;
+
+  # Docker daemon. User is added to the docker group in users.nix so
+  # everyday container work doesn't need sudo.
+  virtualisation.docker.enable = true;
 }
--- nixos/modules/nixos/users.nix
+++ nixos/modules/nixos/users.nix
@@ -6,7 +6,7 @@
   users.users.cardamom = {
     isNormalUser = true;
     description = "Pierre Chanquion";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "docker" ];
     shell = pkgs.zsh;
     packages = with pkgs; [ ];
   };
