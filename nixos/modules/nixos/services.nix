51291ce755154ea67e05384996322f85a6563bbe
Pierre Chanquion <pierre@chanq.io>
2026-04-20 19:57:08 +0100

    feat: enable all terminfo for ssh from ghostty/etc

--- nixos/modules/nixos/services.nix
+++ nixos/modules/nixos/services.nix
@@ -2,4 +2,9 @@
 
 {
   services.openssh.enable = true;
+
+  # Ship terminfo for every terminal emulator we might SSH in from
+  # (ghostty, wezterm, kitty, etc.). Without this, remote sessions fail
+  # with "can't find terminal definition" when TERM isn't xterm-256color.
+  environment.enableAllTerminfo = true;
 }
