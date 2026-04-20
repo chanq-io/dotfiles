3cd039810d15c018cc0085123576800c149b14d7
Pierre Chanquion <pierre@chanq.io>
2026-04-20 22:21:54 +0100

    feat: add gtk xdg portal for gtk app support

--- nixos/modules/nixos/desktop.nix
+++ nixos/modules/nixos/desktop.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.hyprland.enable = true;
+
+  # programs.hyprland already adds xdg-desktop-portal-hyprland; stack the
+  # GTK portal on top so GTK apps (file pickers, Betterbird, Spotify, etc.)
+  # can talk to the portal. common.default = "*" lets GTK serve requests
+  # the hyprland portal doesn't implement.
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
 
   environment.loginShellInit = ''
     if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = "1" ]; then
