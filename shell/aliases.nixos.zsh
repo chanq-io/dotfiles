75c93be8c61659becb2b7211846b100474c1d9ac
Pierre Chanquion <pierre@chanq.io>
2026-05-08 16:06:42 +0100

    feat(shell): add clean-deps alias for nix gc and bootloader refresh

--- shell/aliases.nixos.zsh
+++ shell/aliases.nixos.zsh
@@ -2,4 +2,5 @@
 # ======================
 alias nixos-rebuild='sudo nixos-rebuild switch --flake ~/.dotfiles/nixos#shrike'
 alias update-deps='nix flake update --flake ~/.dotfiles/nixos && nixos-rebuild'
+alias clean-deps='sudo nix-collect-garbage --delete-older-than 7d && sudo /run/current-system/bin/switch-to-configuration boot'
 alias zconf='v ~/.dotfiles/nixos/modules/home/shell.nix'
