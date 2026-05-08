6ac8543b5b9d23f7c0bc3330f1a9caf1ac238b32
Pierre Chanquion <pierre@chanq.io>
2026-05-08 16:05:51 +0100

    fix(shell): correct update-deps alias syntax

--- shell/aliases.nixos.zsh
+++ shell/aliases.nixos.zsh
@@ -1,5 +1,5 @@
 # NixOS-SPECIFIC ALIASES
 # ======================
 alias nixos-rebuild='sudo nixos-rebuild switch --flake ~/.dotfiles/nixos#shrike'
-alias update-deps='nix flake update ~/.dotfiles/nixos && rebuild'
+alias update-deps='nix flake update --flake ~/.dotfiles/nixos && nixos-rebuild'
 alias zconf='v ~/.dotfiles/nixos/modules/home/shell.nix'
